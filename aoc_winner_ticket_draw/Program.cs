5a20b29f95a36acbb8929b70af7aff6eb4f654ad
Daniel Ekroth <irkush@gmail.com>
2022-01-13 08:49:54 +0100

    Initial code commit

---
+++ aoc_winner_ticket_draw/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json.Serialization;
+
+var players = new Dictionary<string, int>()
+{
+    {"Name1", 30 },
+    {"Name2",5},
+    {"Name3", 5},
+    {"Name4",10}
+};
+
+var pool = new Pool();
+
+foreach (var player in players)
+{
+    for (int i = 0; i < player.Value; i++)
+    {
+        pool.AddTicket(player.Key);
+    }
+}
+
+var winner = pool.GetWinnerAndRemoveFromPool();
+Console.WriteLine("First place: " + winner);
+
+var secondPlaceWinner = pool.GetWinnerAndRemoveFromPool();
+Console.WriteLine("Second place: " + secondPlaceWinner);
+
+var thirdPlaceWinner = pool.GetWinnerAndRemoveFromPool();
+Console.WriteLine("Third place: " + thirdPlaceWinner);
+
+class Pool
+{
+    private readonly Random _random = new Random();
+    private readonly List<string> _tickets = new List<string>();
+
+    public string GetWinnerAndRemoveFromPool()
+    {
+        var winningTicket = _random.Next(_tickets.Count - 1);
+        var winner = _tickets[winningTicket];
+
+        // Remove winner.
+        _tickets.RemoveAll(x => x == winner);
+        return winner;
+    }
+
+    public void AddTicket(string ticketName)
+    {
+        _tickets.Add(ticketName);
+    }
+}
